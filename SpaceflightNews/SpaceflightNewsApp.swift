a62e6187b990d15219c2b2e9dcf1a5f81d311bc4
Artem Bolotov <teeemon@gmail.com>
2022-12-18 07:59:43 +0300

    Initial Commit

---
+++ SpaceflightNews/SpaceflightNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceflightNewsApp.swift
+//  SpaceflightNews
+//
+//  Created by artembolotov on 18.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpaceflightNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
